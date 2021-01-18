45a564e3a26e25869e18f0ce217d5c986058aab9
Ondřej Holík <andyholik2@gmail.com>
2021-01-18 21:23:19 +0100

    Initial

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/ondrejholik/telebot
+
+go 1.14
+
+require (
+	github.com/BurntSushi/toml v0.3.1
+	github.com/cweill/gotests v1.6.0 // indirect
+	golang.org/x/mod v0.4.1 // indirect
+	golang.org/x/tools v0.0.0-20210115202250-e0d201561e39 // indirect
+	gopkg.in/tucnak/telebot.v2 v2.3.5
+)
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"log"
+	"time"
+  "fmt"
+
+  "github.com/BurntSushi/toml"
+	tele "gopkg.in/tucnak/telebot.v2"
+)
+
+type Config struct {
+    Token string
+}
+
+func main() {
+  var conf Config
+  if _, err := toml.DecodeFile("config.toml", &conf); err != nil {
+    log.Fatal(err)
+  }
+
+	b, err := tele.NewBot(tele.Settings{
+
+		Token:  conf.Token,
+		Poller: &tele.LongPoller{Timeout: 10 * time.Second},
+	})
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+  // Routes
+
+	b.Handle("/hello", func(m *tele.Message) {
+		b.Send(m.Sender, "Hello World!")
+	})
+
+
+  // SplitWise
+
+  // Weather with GUI
+  b.Handle("/w", func(m *tele.Message) {
+    if weatherGui() {
+
+    } else {
+      b.Send(m.Sender, weather())
+    }
+  })
+
+  // Word Count
+  b.Handle("/wc", func(m *tele.Message) {
+    b.Send(m.Sender, wc(m.Text, true))
+  })
+
+  // QR generator
+
+  // Time ( clock svg/png )
+
+  // Other
+  b.Handle(tele.OnText, func(m *tele.Message) {
+
+  })
+
+
+
+
+	b.Start()
+}
