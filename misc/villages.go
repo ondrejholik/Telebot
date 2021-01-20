63b2dfa8a52a6ac6e1b27e76f850639d9b791e01
Ondřej Holík <andyholik2@gmail.com>
2021-01-20 16:20:38 +0100

    csv loading

--- misc/villages.go
+++ misc/villages.go
@@ -1,6 +1,14 @@
 package telebot
 
-import "github.com/dgraph-io/badger"
+import (
+	"encoding/csv"
+	"io"
+	"log"
+	"strconv"
+	"strings"
+
+	"github.com/dgraph-io/badger"
+)
 
 // Village name, gps point lat, lon
 type Village struct {
@@ -30,6 +38,34 @@ func ClosestVillage(db *badger.DB, p Point) string {
 	return "a"
 }
 
-func LoadVillages(db *badger.DB) {
+func ParseToVillage(row []string) Village {
+	return Village{row[0], Point{Lat: strconv.Itoa(row[1]), Lon: strconv.Itoa(row[2])}}
+}
 
+func LoadVillagesToDB(db *badger.DB) {
+	var villages []Village
+
+	r := csv.NewReader(strings.NewReader(in))
+	r.Comma = ';'
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		village := ParseToVillage(record)
+		villages = append(villages, village)
+	}
+
+	err := db.Update(func(txn *badger.Txn) error {
+		err := txn.Set([]byte("villages"), []byte(json.Marshall(villages)))
+		return err
+	})
+	if err != nil {
+		log.Panic(err)
+	}
 }
